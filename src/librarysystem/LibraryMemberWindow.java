b2014216f6abe8c5cf2259bcc98f72eea42be0a8
Gebreegziabher Gebregiorgis <3g.mit02@gmail.com>
2022-12-08 12:23:19 -0600

    member info added to table

--- src/librarysystem/LibraryMemberWindow.java
+++ src/librarysystem/LibraryMemberWindow.java
@@ -543,15 +543,23 @@ public class LibraryMemberWindow extends javax.swing.JFrame {
 
         jTable1.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Member ID", "First Name", "Last Name", "Telephone", "Street Address", "State", "City", "Zip Code"
             }
-        ));
+        ) {
+            Class[] types = new Class [] {
+                java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class, java.lang.String.class
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+        });
         jScrollPane1.setViewportView(jTable1);
 
         javax.swing.GroupLayout jPanel5Layout = new javax.swing.GroupLayout(jPanel5);
